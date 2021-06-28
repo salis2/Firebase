f465f5a53819e60c87fcdff1e56d4ee78edea7f8
salis2 <56523603+salis2@users.noreply.github.com>
2021-06-28 16:56:53 +0700

    readData

---
+++ app/src/main/java/com/example/firebase/adapter/AdapterLihatTeman.java
@@ -0,0 +1,54 @@
+package com.example.firebase.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firebase.R;
+import com.example.firebase.database.Teman;
+
+import java.util.ArrayList;
+
+public class AdapterLihatTeman extends RecyclerView.Adapter<AdapterLihatTeman.ViewHolder> {
+
+    private ArrayList<Teman> daftarTeman;
+    private Context context;
+
+    public AdapterLihatTeman(ArrayList<Teman> daftarTeman, Context context) {
+        this.daftarTeman = daftarTeman;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_teman,parent,false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String nama = daftarTeman.get(position).getNama();
+        holder.tvnama.setText(nama);
+    }
+
+    @Override
+    public int getItemCount() {
+        return daftarTeman.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvnama;
+
+        ViewHolder(View v){
+            super(v);
+            tvnama = (TextView) v.findViewById(R.id.tv_nama);
+        }
+    }
+}
